c829134867be60000766a0cde12d6c85af83d4a9
ccallow <71406811+ccallow@users.noreply.github.com>
2020-09-28 12:24:26 -0700

    v

--- problem3.py
+++ problem3.py
@@ -6,3 +6,22 @@
 # If they get the password correct (password is 12345password) 
 # then display the message "Access granted"
 # 1 marks
+
+print("=============================")
+username=input("Enter a username: ")
+print("=============================")
+user=str(username)
+b="admin"
+b=str(b)
+if b in user:
+  pw=input("Password: ")
+  pw=str(pw)
+  a="12345password"
+  a=str(a)
+  if pw is a:
+    print("Access granted")
+
+else:
+  print("invalid user")
+print("=============================\n\n\n")
+
