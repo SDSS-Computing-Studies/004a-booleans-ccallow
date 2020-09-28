06f136776dd236e9d3652aad09dbb1d94a64513b
ccallow <71406811+ccallow@users.noreply.github.com>
2020-09-28 12:05:20 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -10,3 +10,14 @@
 # Outputs:
 # "the number is an integer"
 # "the number is not an integer"
+print("=============================")
+num=input("Enter a number: ")
+print("=============================")
+num=float(num)
+
+if int(num) == num:
+  print("the number is an integer")
+else:
+  print("the number is not an integer")
+print("=============================\n\n\n")
+
